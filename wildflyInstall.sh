dcca4f465879eceec44b6a5f198afd0f326c053e
Daniel Little <daniel.r.little.1994@gmail.com>
2019-06-28 13:54:59 +0100

    edited the install doc again

--- wildflyInstall.sh
+++ wildflyInstall.sh
@@ -3,6 +3,7 @@
 sudo apt install -y wget vim openjdk-8-jdk openjdk-8-jre
 
 sudo useradd --create-home wildfly
+sudo usermod --shell /bin/bash wildfly
 
 sudo su - wildfly -c "wget https://download.jboss.org/wildfly/17.0.0.Final/wildfly-17.0.0.Final.tar.gz"
 
