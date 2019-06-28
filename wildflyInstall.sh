8af09a10c3ad17616d7f96ffdda548e01c2f396f
Daniel Little <daniel.r.little.1994@gmail.com>
2019-06-28 13:58:14 +0100

    edited the install doc again

--- wildflyInstall.sh
+++ wildflyInstall.sh
@@ -7,7 +7,7 @@ sudo usermod --shell /bin/bash wildfly
 
 sudo su - wildfly -c "wget https://download.jboss.org/wildfly/17.0.0.Final/wildfly-17.0.0.Final.tar.gz"
 
-sudo su - wildfly -c "tar -xf wildfly-17.0.0.Final.tar.gz"
+sudo su - wildfly -c "tar -xf ./wildfly-17.0.0.Final.tar.gz"
 
 sudo mv wildfly.service /etc/systemd/system/wildfly.service
 
