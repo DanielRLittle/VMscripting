2e8b6d06c6bf0f99513ff9848ad9e1c84b2731bf
Daniel Little <daniel.r.little.1994@gmail.com>
2019-07-25 11:58:03 +0100

    removed need for sudo in second docker script and added sleep for mongo install

--- dockerRunAngProject.sh
+++ dockerRunAngProject.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-sudo docker pull danielrlittle/api
-sudo docker pull danielrlittle/ui
+docker pull danielrlittle/api
+docker pull danielrlittle/ui
 
-sudo docker network create pool-app
+docker network create pool-app
 
-sudo docker run -d --network pool-app --name mongo mongo
-sudo docker run -d --network pool-app --name api danielrlittle/api
-sudo docker run -d -p 80:80 --network pool-app --name ui danielrlittle/ui
+docker run -d --network pool-app --name mongo mongo
+sleep 5
+docker run -d --network pool-app --name api danielrlittle/api
+docker run -d -p 80:80 --network pool-app --name ui danielrlittle/ui
+
