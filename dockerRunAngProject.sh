e8d18e7f7c33a9a4de8c33a9c5c611e337472f19
Daniel Little <daniel.r.little.1994@gmail.com>
2019-07-25 11:43:05 +0100

    opening port 80 for angular project

--- dockerRunAngProject.sh
+++ dockerRunAngProject.sh
@@ -7,4 +7,4 @@ sudo docker network create pool-app
 
 sudo docker run -d --network pool-app --name mongo mongo
 sudo docker run -d --network pool-app --name api danielrlittle/api
-sudo docker run -d --network pool-app --name ui danielrlittle/ui
+sudo docker run -d -p 80:80 --network pool-app --name ui danielrlittle/ui
