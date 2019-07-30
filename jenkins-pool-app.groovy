08168bf171ef1845e2a037aa0fc65c95473684a9
Daniel Little <daniel.r.little.1994@gmail.com>
2019-07-30 16:22:15 +0100

    added docker install and apt update

--- jenkins-pool-app.groovy
+++ jenkins-pool-app.groovy
@@ -1,5 +1,9 @@
 node {
+    stage("update") {
+        sh "sudo apt update"
+    }
     stage("run ui") {
+        sh "curl https://get.docker.com | sudo bash"
         sh "sudo docker run -d -p 80:80 danielrlittle/ui2"
     }
 }
